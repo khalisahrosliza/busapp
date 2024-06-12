a0ecc1f214da54d08fcce5f4b7facab49fbfd6f6
nadirah <nadirahros.liza@live.iium.edu.my>
2024-06-12 15:55:03 +0800

    updated

---
+++ busapp/app/Http/Controllers/BusAController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BusAController extends Controller
+{
+    public function index()
+{
+    return view('bus_a');
+}
+
+}
