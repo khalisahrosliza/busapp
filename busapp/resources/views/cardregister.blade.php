fb395c12e260b0e733f6e96a96ec9ebb2839646d
KHALISAH ROS LIZA <129260321+khalisahrosliza@users.noreply.github.com>
2024-06-21 12:43:30 +0800

    update

--- busapp/resources/views/cardregister.blade.php
+++ busapp/resources/views/cardregister.blade.php
@@ -1,5 +1,22 @@
 @extends('navbar')
 @section('content')
+
+<?php
+/*require "dbconn.php";
+
+if (isset($_POST["card_id"])) {   // rfid posted as a query
+    $card_id = $_POST["card_id"];
+    $std_id = $_POST["std_id"];
+    $faculty = $_POST["faculty"];
+    $gender = $_POST["gender"];
+    $residential = $_POST["residential"];
+     $sql = "INSERT INTO users (rfid_uid, name, gender, faculty, student_type) VALUES ('" . $card_id . "', '" . $std_id . "', '" . $gender . "', '" . $faculty . "', '" . $residential ."')";
+    if ($conn->query($sql) === TRUE) ;
+
+}*/
+
+?>
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -9,11 +26,26 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('card-register') }}">
                         @csrf
+
+                        <div class="row mb-3">
+                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('CARD ID') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="card_id" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
                         <div class="row mb-3">
                             <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('STUDENT ID') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="std_id" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
--- busapp/resources/views/navbar.blade.php
+++ busapp/resources/views/navbar.blade.php
@@ -90,7 +90,7 @@
             <div class="col-sm-6 clearfix">
                 <div class="user-profile pull-right">
                     <img class="avatar user-thumb" src="{{asset('import/assets/images/author/avatar.png')}}" alt="avatar">
-                    <h4 class="user-name dropdown-toggle" data-toggle="dropdown">khalisah <i class="fa fa-angle-down"></i></h4>
+                    <h4 class="user-name dropdown-toggle" data-toggle="dropdown">Abdul <i class="fa fa-angle-down"></i></h4>
                     <div class="dropdown-menu">
                         <a class="dropdown-item" href="#">Message</a>
                         <a class="dropdown-item" href="#">Settings</a>
